c03178f0e44fc2b63e61bc984032a725d6fcd418
Niki Micallef <nikmic94@gmail.com>
2021-06-22 21:26:36 +0200

    - Initial commit containing structure - Initial API version

---
+++ src/main/java/com/rbmh/contentbrowser/ContentBrowserApplication.java
@@ -0,0 +1,13 @@
+package com.rbmh.contentbrowser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentBrowserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentBrowserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rbmh/contentbrowser/ContentBrowserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rbmh.contentbrowser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentBrowserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
